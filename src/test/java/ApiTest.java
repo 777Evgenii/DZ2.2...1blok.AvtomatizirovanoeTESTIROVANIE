b5c8b3f9ca7e42b9fbdcdfde5e4fbe7976825f51
Evgenii <mikhailovich21@mail.ru>
2023-05-21 18:45:38 +0500

    init

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class ApiTest {
+    @Test
+    void testEcho() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("1993") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("1993"))
+        ;
+    }
+}
+
